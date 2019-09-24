18a87bf566b5976faf50c04f4992f608ad388412
Samuel Chordas <samec39@gmail.com>
2019-09-24 12:29:49 -0700

    Default RSA to passed public key Separated RSA and AES encryption requestions RSA encryption no longer requires a keyAlias RSA encryption calls require a public key Demoing note RSA function by retrieving apps own public key

--- saltblock/src/main/java/io/mjolnir/saltblock/ProcessAESEncryptionRequest.kt
+++ saltblock/src/main/java/io/mjolnir/saltblock/ProcessAESEncryptionRequest.kt
@@ -7,32 +7,32 @@ import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.Serializable
 import java.security.Key
-import javax.crypto.Cipher
 import javax.crypto.SecretKey
 
-fun processEncryptionRequest(encryptionAlgorithm: EncryptionAlgorithm, keyAlias: String,
-                             plainTexts: List<String>): List<String> {
+fun processEncryptionRequest(keyAlias: String,
+                             plainTexts: List<String>):
+        List<String> {
     return runBlocking {
         withContext(Dispatchers.Default) {
-            threadedEncryptionRequest(encryptionAlgorithm, keyAlias, plainTexts)
+            threadedEncryptionRequest(keyAlias, plainTexts)
         }
     }
 }
 
-fun processEncryptionRequest(encryptionAlgorithm: EncryptionAlgorithm, keyAlias: String,
+fun processEncryptionRequest(keyAlias: String,
                              plainText: String) : String {
     return runBlocking {
         withContext(Dispatchers.Default) {
-            threadedEncryptionRequest(encryptionAlgorithm, keyAlias, plainText)
+            threadedEncryptionRequest(keyAlias, plainText)
         }
     }
 }
 
-fun processEncryptionRequest(encryptionAlgorithm: EncryptionAlgorithm, keyAlias: String,
+fun processEncryptionRequest(keyAlias: String,
                              obj: Serializable) : String {
     return runBlocking {
         withContext(Dispatchers.Default) {
-            threadedEncryptionRequest(encryptionAlgorithm, keyAlias, obj)
+            threadedEncryptionRequest(keyAlias, obj)
         }
     }
 }
@@ -71,14 +71,6 @@ private fun threadedShareFileEncryptionRequest(key: SecretKey, plainBytes: ByteA
     }
 }
 
-private fun threadedKeyRequest() : SecretKey {
-    return AES.getKeyToWrap()
-}
-
-private fun threadedKeyWrapRequest(publicKey: String, key: Key) : String {
-    return RSA.wrapKey(publicKey, key)
-}
-
 private fun threadedEncryptionRequest(keyAlias: String,
                                       plainBytes: ByteArray) : String {
     return try {
@@ -89,40 +81,22 @@ private fun threadedEncryptionRequest(keyAlias: String,
     }
 }
 
-private fun threadedEncryptionRequest(encryptionAlgorithm: EncryptionAlgorithm, keyAlias: String,
+private fun threadedEncryptionRequest(keyAlias: String,
                               plainText: String) : String {
     val plainBytes = plainText.toByteArray()
     return try {
-        when(encryptionAlgorithm) {
-            EncryptionAlgorithm.AES -> {
-                AES.encrypt(keyAlias, plainBytes)
-            }
-            EncryptionAlgorithm.RSA -> {
-                // Check bytes are not over 245
-                isOver(plainBytes)
-                encryptRSA(keyAlias, plainBytes)
-            }
-        }
+        AES.encrypt(keyAlias, plainBytes)
     } catch (e : Exception) {
         e.printStackTrace()
         emptyString()
     }
 }
 
-private fun threadedEncryptionRequest(encryptionAlgorithm: EncryptionAlgorithm, keyAlias: String,
+private fun threadedEncryptionRequest(keyAlias: String,
                               obj: Serializable) : String {
     val plainBytes = obj.getByteArray()
     return try {
-        when(encryptionAlgorithm) {
-            EncryptionAlgorithm.AES -> {
-                AES.encrypt(keyAlias, plainBytes)
-            }
-            EncryptionAlgorithm.RSA -> {
-                // Check bytes are not over 245
-                isOver(plainBytes)
-                encryptRSA(keyAlias, plainBytes)
-            }
-        }
+        AES.encrypt(keyAlias, plainBytes)
     } catch (e : Exception) {
         e.printStackTrace()
         emptyString()
@@ -130,24 +104,20 @@ private fun threadedEncryptionRequest(encryptionAlgorithm: EncryptionAlgorithm, 
 
 }
 
-private fun threadedEncryptionRequest(encryptionAlgorithm: EncryptionAlgorithm, keyAlias: String,
-                              plainTexts: List<String>): List<String> {
+private fun threadedEncryptionRequest(keyAlias: String,
+                              plainTexts: List<String>) : List<String> {
     return try {
-        when(encryptionAlgorithm) {
-            EncryptionAlgorithm.AES -> {
-                AES.encrypt(keyAlias, plainTexts)
-            }
-            EncryptionAlgorithm.RSA -> {
-                RSA.encrypt(keyAlias, plainTexts)
-            }
-        }
+        AES.encrypt(keyAlias, plainTexts)
     } catch (e : Exception) {
         e.printStackTrace()
         emptyList()
     }
 }
 
-private fun encryptRSA(keyAlias: String, plainBytes: ByteArray) : String {
-    val cipherBytes = RSA.doFinal(Cipher.ENCRYPT_MODE, keyAlias, plainBytes)
-    return Encoder.encodeToString(cipherBytes)
+private fun threadedKeyRequest() : SecretKey {
+    return AES.getKeyToWrap()
+}
+
+private fun threadedKeyWrapRequest(publicKey: String, key: Key) : String {
+    return RSA.wrapKey(publicKey, key)
 }
--- saltblock/src/main/java/io/mjolnir/saltblock/RSA.java
+++ saltblock/src/main/java/io/mjolnir/saltblock/RSA.java
@@ -45,12 +45,13 @@ class RSA extends RSAKeyProvider {
         return cipher.unwrap(wrappedKeyBytes, "AES", Cipher.SECRET_KEY);
     }
 
-    static List<String> encrypt(String keyAlias, List<String> plainTexts) throws
+    static List<String> encrypt(List<String> plainTexts, String publicKey) throws
             NoSuchPaddingException, NoSuchAlgorithmException, NoSuchProviderException,
-            InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-        PublicKey key = getPublicKey(keyAlias);
+            InvalidKeyException, BadPaddingException, IllegalBlockSizeException,
+            InvalidKeySpecException {
+        PublicKey key = publicKeyFromString(publicKey);
 
-        Cipher cipher = Cipher.getInstance(Constants.RSA, Constants.KEY_STORE_WORK_AROUND);
+        Cipher cipher = Cipher.getInstance(Constants.RSA);
         cipher.init(Cipher.ENCRYPT_MODE, key);
 
         List<String> cipherTexts = new ArrayList<>();
@@ -98,12 +99,12 @@ class RSA extends RSAKeyProvider {
         return cipher.doFinal(cipherBytes);
     }
 
-    static byte[] doFinal(int mode, String alias, byte[] bytes) throws NoSuchPaddingException,
-            NoSuchAlgorithmException, NoSuchProviderException, InvalidKeyException,
-            BadPaddingException, IllegalBlockSizeException {
+    static byte[] doFinal(int mode, String alias, byte[] bytes, String publicKey) throws
+            NoSuchPaddingException, NoSuchAlgorithmException, NoSuchProviderException,
+            InvalidKeyException, BadPaddingException, IllegalBlockSizeException, InvalidKeySpecException {
         Key key;
         if (mode == Cipher.ENCRYPT_MODE) {
-           key = getPublicKey(alias);
+            key = publicKeyFromString(publicKey);
         } else if(mode == Cipher.DECRYPT_MODE) {
            key = getPrivateKey(alias);
         } else {
