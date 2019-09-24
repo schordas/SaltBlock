f0fc198c34d07a793b5c41b29dada7618cd03648
Samuel Chordas <samec39@gmail.com>
2019-09-24 14:46:44 -0700

    RSA can be retrieved without alg check

--- saltblock/src/main/java/io/mjolnir/saltblock/ProcessKeyRequest.kt
+++ saltblock/src/main/java/io/mjolnir/saltblock/ProcessKeyRequest.kt
@@ -5,11 +5,7 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import java.security.PublicKey
 
-fun processPublicKeyRequest(encryptionAlgorithm: EncryptionAlgorithm,
-                            keyAlias: String) : String {
-   require(encryptionAlgorithm == EncryptionAlgorithm.RSA) {
-       "You can only share an RSA public key, please set your encryption algorithm accordingly"
-   }
+fun processPublicKeyRequest(keyAlias: String) : String {
     return runBlocking {
         withContext(Dispatchers.Default) {
             val key = threadedPublicKeyRequest(keyAlias)
